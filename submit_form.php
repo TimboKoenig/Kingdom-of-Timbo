ef1ddac1d1bccae81a871158b6cc042de2fbfc02
TimboKoenig <timbokoenig77@gmail.com>
2024-11-26 22:08:18 +0100

    Update submit_form.php

--- submit_form.php
+++ submit_form.php
@@ -1,18 +1,14 @@
 <?php
-// Daten aus dem Formular abrufen
 $name = $_POST['name'];
 $profession = $_POST['profession'];
 $institution = $_POST['institution'];
 $email = $_POST['email'];
 $anonymous = isset($_POST['anonymous']) ? "Anonym" : $name;
-$newsletter = isset($_POST['newsletter']) ? "Ja" : "Nein";
 
-// Daten in einer Textdatei speichern
 $file = fopen("members.txt", "a");
-fwrite($file, "$anonymous,$profession,$institution,$email,$newsletter\n");
+fwrite($file, "$anonymous,$profession,$institution\n");
 fclose($file);
 
-// Bestätigungsnachricht anzeigen
 echo "<h1>Danke für deine Anmeldung!</h1>";
 echo "<p>Wir haben deine Daten erhalten. Du bist nun Teil des Kingdom of Timbo.</p>";
 echo "<p><a href='index.html'>Zurück zur Startseite</a></p>";
